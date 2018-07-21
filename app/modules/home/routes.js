bd9dce1e98b863cd0eeb43ddd58ff92de715e6b9
joshuaraemacaya <macaya737@gmail.com>
2018-07-21 18:12:12 +0800

    ookoo

--- app/modules/home/routes.js
+++ app/modules/home/routes.js
@@ -3,18 +3,17 @@ var indexRouter = express.Router();
 var authMiddleware = require('../../modules/auth/middlewares/auth');
 var db = require('../../lib/database')();
 
-indexRouter.route('/').get(authMiddleware.noAuthed, (req, res) => {
+indexRouter.use(authMiddleware.noAuthed)
+indexRouter.get('/', (req, res) => {
   var queryString1 =`SELECT * FROM tbl_event where var_eventname = 'Baptism' or var_eventname = 'Funeral Service' or var_eventname = 'Marriage' or var_eventname = 'Facility Reservation' or var_eventname = 'Document Request' or var_eventname = 'Establishment Blessing' `
   db.query(queryString1, (err, results, fields) => {
     var events =results;
       if (err) console.log(err);
-      
-  
       return res.render('home/views/index',{ events : events });
   });
 
 })
-indexRouter.post('/query',authMiddleware.noAuthed, (req, res) => {
+indexRouter.post('/query', (req, res) => {
   var queryString =`SELECT * FROM tbl_event where int_eventID = ? `
   db.query(queryString,[req.body.id], (err, results, fields) => {
       if (err) console.log(err);
@@ -24,10 +23,10 @@ indexRouter.post('/query',authMiddleware.noAuthed, (req, res) => {
 
 })
 
-indexRouter.route('/schedule')
-.get(authMiddleware.noAuthed, (req, res) => {
+indexRouter.get('/schedule', (req, res) => {
+
     res.render('home/views/schedule', req.query);
-})
+});
 
 
 exports.index = indexRouter;
