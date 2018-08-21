1a55e6aa9c99b22920eb82c0b06661e8d3901769
joycetonzon <Najtonzon06@gmail.com>
2018-08-22 01:08:22 +0800

    jj

--- app/modules/home/routes.js
+++ app/modules/home/routes.js
@@ -5,7 +5,7 @@ var db = require('../../lib/database')();
 
 indexRouter.use(authMiddleware.noAuthed)
 indexRouter.get('/', (req, res) => {
-  var queryString1 =`SELECT * FROM tbl_services where var_eventname = 'Baptism' or var_eventname = 'Funeral Service' or var_eventname = 'Marriage' or var_eventname = 'Facility Reservation' or var_eventname = 'Document Request' or var_eventname = 'Establishment Blessing' `
+  var queryString1 =`SELECT * FROM tbl_services where var_eventname = 'Baptism' or var_eventname = 'Funeral Service' or var_eventname = 'Marriage' or var_eventname = 'Facility Reservation' or var_eventname = 'Document Request' or var_eventname = 'Establishment Blessing'  or var_eventname = 'Anointing of the sick'`
   db.query(queryString1, (err, results, fields) => {
     var events =results;
       if (err) console.log(err);
@@ -25,9 +25,37 @@ indexRouter.post('/query', (req, res) => {
   })
 })
 
+indexRouter.get('/marriagedetails', (req, res) => {
+  var queryString1 =`SELECT * FROM tbl_wedsteps`
+  db.query(queryString1, (err, results, fields) => {
+    if (err) console.log(err);
+    var steps= results;
+    console.log(steps)
+    var queryString2 =`select * from tbl_requirementtype where int_eventID = (select int_eventID from tbl_services where var_eventname='Marriage')`
+    db.query(queryString2, (err, results, fields) => {
+      if (err) console.log(err);
+      var requirements= results;
+
+      var queryString3 =`SELECT var_eventdesc FROM tbl_services where var_eventname = 'Marriage'`
+      db.query(queryString3, (err, results, fields) => {
+        var desc =results;
+        
+      var queryString4 =`SELECT var_eventdesc FROM tbl_services where var_eventname = 'Marriage'`
+      db.query(queryString4, (err, results, fields) => {
+        var events =results;
+        
+  
+
+      return res.render('home/views/marriage1', {steps: steps, requirements: requirements, description:desc, events: events});
+    });
+    });
+  });
+  });
+});
+
 indexRouter.get('/schedule', (req, res) => {
 
-    res.render('home/views/schedule', req.query);
+  res.render('home/views/schedule', req.query);
 });
 
 indexRouter.get('/services', (req, res) => {
