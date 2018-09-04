aba6e5d138014d5ba8003a3c5e22fad8e44f84e6
jonalynfe11 <jonalynfeebrada11@gmail.com>
2018-09-04 21:06:07 +0800

    catechist for merge

--- app/modules/catechist/routes.js
+++ app/modules/catechist/routes.js
@@ -12,6 +12,14 @@ catechistRouter.get('/', (req, res)=>{
 });
 
 
-
+catechistRouter.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500)
+    return res.render('catechist/views/error/505', {title: '505: Something broke!'});
+  })
+catechistRouter.use(function(req, res, next) {
+    res.status(404)
+    return res.render('catechist/views/error/404', {title: '404: File Not Found'});
+});
 //===============================================================================================//
 exports.catechist = catechistRouter;
